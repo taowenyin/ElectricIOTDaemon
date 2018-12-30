cbb6f4901d059c8dfcd843acb2510c08fdd9c7de
wenyin.tao <wenyin.tao@163.com>
2018-12-30 19:29:05 +0800

    分离所有的配置项

---
+++ src/main/java/siso/edu/cn/GlobalSetting.java
@@ -0,0 +1,15 @@
+package siso.edu.cn;
+
+public class GlobalSetting {
+
+    public static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/electric_iot?useSSL=true&serverTimezone=Asia/Shanghai";
+    public static final String DB_USER = "root";
+    public static final String DB_PASSWORD = "05200902";
+    public static final String CHECKING_SQL = "SELECT * FROM electric_iot.device_location WHERE " +
+            "electric_iot.device_location.longitude IS NULL OR " +
+            "electric_iot.device_location.latitude IS NULL";
+
+    public static final String SERVE_URL = "http://localhost:8080/api/manage/location";
+
+}
--- src/main/java/siso/edu/cn/LocationChecking.java
+++ src/main/java/siso/edu/cn/LocationChecking.java
@@ -10,27 +10,17 @@ import java.sql.Connection;
 
 public class LocationChecking {
 
-    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/electric_iot?useSSL=true&serverTimezone=Asia/Shanghai";
-    private static final String DB_USER = "root";
-    private static final String DB_PASSWORD = "05200902";
-    private static final String CHECKING_SQL = "SELECT * FROM electric_iot.device_location WHERE " +
-            "electric_iot.device_location.longitude IS NULL OR " +
-            "electric_iot.device_location.latitude IS NULL";
-
-    private static final String SERVE_URL = "http://localhost:8080/api/manage/location";
-
     private static final OkHttpClient client = new OkHttpClient();
 
     public static void Checking() throws ClassNotFoundException, SQLException, IOException {
         // 注册JDBC驱动
-        Class.forName(JDBC_DRIVER);
+        Class.forName(GlobalSetting.JDBC_DRIVER);
         // 打开数据库链接
-        Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        Connection connection = DriverManager.getConnection(GlobalSetting.DB_URL, GlobalSetting.DB_USER, GlobalSetting.DB_PASSWORD);
         // 实例化Statement对象
         Statement statement = connection.createStatement();
         // 获取查询的结果
-        ResultSet resultSet = statement.executeQuery(CHECKING_SQL);
+        ResultSet resultSet = statement.executeQuery(GlobalSetting.CHECKING_SQL);
 
         while (resultSet.next()) {
             System.out.println(String.format("ID = %d is checking location", resultSet.getInt("id")));
@@ -38,7 +28,7 @@ public class LocationChecking {
             // 构建提交数据的Body
             RequestBody body = new FormBody.Builder().add("id", String.valueOf(resultSet.getInt("id"))).build();
             // 构建提交HTTP PUT的请求
-            Request request = new Request.Builder().url(SERVE_URL).put(body).build();
+            Request request = new Request.Builder().url(GlobalSetting.SERVE_URL).put(body).build();
 
             // 提交HTTP请求
             client.newCall(request).enqueue(new Callback() {
