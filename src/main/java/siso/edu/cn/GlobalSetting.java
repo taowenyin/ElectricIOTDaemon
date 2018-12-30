15692973c0a8b17e383f7e294e326001b177dafd
wenyin.tao <wenyin.tao@163.com>
2018-12-30 20:15:04 +0800

    每次只查10行结果

--- src/main/java/siso/edu/cn/GlobalSetting.java
+++ src/main/java/siso/edu/cn/GlobalSetting.java
@@ -8,7 +8,7 @@ public class GlobalSetting {
     public static final String DB_PASSWORD = "05200902";
     public static final String CHECKING_SQL = "SELECT * FROM electric_iot.device_location WHERE " +
             "electric_iot.device_location.longitude IS NULL OR " +
-            "electric_iot.device_location.latitude IS NULL";
+            "electric_iot.device_location.latitude IS NULL LIMIT 10";
 
     public static final String SERVE_URL = "http://localhost:8080/api/manage/location";
 
